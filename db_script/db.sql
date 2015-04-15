9ddf7fe2345931abf43e6f858f82a2dd84d12fa0
o1egkl <o1egkl@gmail.com>
2015-04-15 20:31:23 +0300

    db fix

--- db_script/db.sql
+++ db_script/db.sql
@@ -1,16 +1,16 @@
 CREATE DATABASE IF NOT EXISTS `admin`;
 
 CREATE TABLE IF NOT EXISTS `admin`.`users` (
-  `user_id` int(11) NOT NULL AUTO_INCREMENT,
-  `user_name` varchar(64) COLLATE utf8_unicode_ci NOT NULL ,
-  `user_password_hash` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `user_email` varchar(64) COLLATE utf8_unicode_ci NOT NULL COMMENT,
+  `user_id` INT(11) NOT NULL AUTO_INCREMENT ,
+  `user_name` VARCHAR(64) NOT NULL,
+  `user_password_hash` VARCHAR(255) NOT NULL,
+  `user_email` VARCHAR(64) NOT NULL ,
   PRIMARY KEY (`user_id`),
   UNIQUE KEY `user_name` (`user_name`),
   UNIQUE KEY `user_email` (`user_email`)
-) DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci ;
+) ENGINE=INNODB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci ;
 
-CREATE TABLE IF NOT EXISTS `tracker` (
+CREATE TABLE IF NOT EXISTS `admin`.`tracker` (
 `id` INT(11) NOT NULL AUTO_INCREMENT,
 `date` DATE NOT NULL,
 `time` TIME NOT NULL,
